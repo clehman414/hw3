080b44dc1be9aba9f9c038834ac507f8b5b84182
Caitlin Lehman <caitlin.lehman@kellogg.northwestern.edu>
2025-02-25 17:26:13 +0000

    Sets up Entries index/new views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,8 +5,16 @@ class EntriesController < ApplicationController
   end
   
   def new
+    @place = Place.find_by({ "id" => params["place_id"] })
   end
 
   def create
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["place_id"] = params["place_id"]
+    @contact.save
+    redirect_to "/places/#{@entry["place_id"]}"
   end
 end
